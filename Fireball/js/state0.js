0eae411a5164ec8ed67cabbadcd5322e694e8994
Matthew Wilkinson <matthews@tlccompanies.com>
2018-03-28 16:59:06 -0500

    added mild animation for zordo and added haydrool background

--- Fireball/js/state0.js
+++ Fireball/js/state0.js
@@ -11,8 +11,14 @@ demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
         game.load.image('zordo', 'assets/Sprites/Zordo.png');
+        game.load.image('sky', 'assets/Haydrool.png');
     },
     create: function(){
+        
+        // initialize the game physics engine
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        // locally scoped background variable
+        var sky = game.add.sprite(0, 0, 'sky');
         // event listeners are local to the state they're in.
         addChangeStateEventListeners();
         
@@ -23,17 +29,33 @@ demo.state0.prototype = {
         zordo = game.add.sprite(centerX, centerY, 'zordo');
         // anchor x/y of the image to the center, not the top left.
         zordo.anchor.setTo(0.5, 0.5);
+        // set zordo's scale
+        zordo.scale.setTo(0.7, 0.7);
+        game.physics.enable(zordo);
+        zordo.body.collideWorldBounds = true;
+        // initialize the game bounds
+        game.world.setBounds(0, 0, 3000, 1000);
+
+        // get the camera to follow zordo
+        game.camera.follow(zordo);
+        // create deadzone for the camera (600 x 1000)
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            zordo.scale.setTo(0.7, 0.7);
             zordo.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            zordo.scale.setTo(-0.7, 0.7);
             zordo.x -= speed;
         }
         
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            zordo.y -= speed;
+            // only reduce y if zordos.y >= 335~      [zordo.y in the console]
+            if (zordo.y >= 335){
+                zordo.y -= speed;   
+            }
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             zordo.y += speed;
