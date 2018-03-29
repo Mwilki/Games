68910407bd5d049fdb205c59a164f50de9a6d819
Matthew Wilkinson <matthews@tlccompanies.com>
2018-03-29 15:18:58 -0500

    added sprite animation

--- Fireball/js/state0.js
+++ Fireball/js/state0.js
@@ -6,12 +6,14 @@ var speed = 5;
 
 // initialize player
 var zordo;
+var gooblin;
 
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
-        game.load.image('zordo', 'assets/Sprites/Zordo.png');
+        game.load.spritesheet('zordo', 'assets/Sprites/ZordoSpritesheet.png', 126, 162);
         game.load.image('sky', 'assets/Haydrool.png');
+        game.load.image('gooblin', 'assets/Sprites/Gooblin.png');
     },
     create: function(){
         
@@ -25,14 +27,25 @@ demo.state0.prototype = {
         // scaling manager
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
+        // initialize gooblin into the world (TESTING)
+//        gooblin = game.add.sprite(centerX + 100, centerY + 100, 'gooblin');
+//        gooblin = game.add.sprite(centerX - 100, centerY - 100, 'gooblin');
         // initialize zordo into the world.
         zordo = game.add.sprite(centerX, centerY, 'zordo');
+        zordo.animations.add('walk', [1, 2]);
         // anchor x/y of the image to the center, not the top left.
         zordo.anchor.setTo(0.5, 0.5);
         // set zordo's scale
         zordo.scale.setTo(0.7, 0.7);
         game.physics.enable(zordo);
         zordo.body.collideWorldBounds = true;
+        
+        gooblin = game.add.sprite(centerX + zordo.x, centerY - 350, 'gooblin');
+        game.physics.enable(gooblin, Phaser.Physics.ARCADE);
+        gooblin.collideWorldBounds = true;
+        gooblin.enableBody = true;
+
+        
         // initialize the game bounds
         game.world.setBounds(0, 0, 3000, 1000);
 
@@ -42,13 +55,40 @@ demo.state0.prototype = {
         game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function(){
+        
+
+while (gooblin.y !== zordo.y && gooblin.x !== zordo.x){
+    if (gooblin.y >= zordo.y){
+        gooblin.y -= speed;
+    } else {
+        gooblin.y += speed;
+    }
+    
+    if (gooblin.x >= zordo.x){
+        gooblin.x -= speed;
+    } else {
+        gooblin.x += speed;
+    }
+
+}
+
+        
+        
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        zordo.animations.play('walk', 20, true);
+        zordo.animations.currentAnim.speed = 5;
             zordo.scale.setTo(0.7, 0.7);
             zordo.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            zordo.animations.play('walk', 20, true);
+            zordo.animations.currentAnim.speed = 5;
             zordo.scale.setTo(-0.7, 0.7);
             zordo.x -= speed;
+        }
+        else {
+            zordo.animations.stop('walk');
+            zordo.frame = 0;
         }
         
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
